943ec317d790edf0da74f27f3a7637defd11dc57
Fabriel <ocr@loubier.fr>
2017-06-14 19:44:19 +0200

    AppKernel modified (AppBundle removed)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new P4Louvre\TicketsBundle\P4LouvreTicketsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
