02176ebd389f32458d5018be3574c9e409305753
Fabriel <ocr@loubier.fr>
2017-06-29 14:58:02 +0200

    Automically add slashes in visitor's date of birth removed

--- web/js/addSlashes.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.js-dob').keyup(function(){
-        var nbChar = $(this).val().length;
-        if(nbChar == 2 || nbChar == 5) {
-            $(this).val($(this).val()+'/');
-        }
-    });
-});
